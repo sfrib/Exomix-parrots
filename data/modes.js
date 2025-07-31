0d93fb88e1f77435f99de962a70ffbc1e315590e
sfrib <sebastianfranco.py@gmail.com>
2025-07-31 12:30:15 +0200

    Create modes.js

---
+++ data/modes.js
@@ -0,0 +1,63 @@
+[
+  {
+    "mode": "běžný stav",
+    "adjustments": {
+      "protein": [10, 18],
+      "fat": [5, 15],
+      "fiber": [5, 10],
+      "calcium": [0.8, 1.5],
+      "phosphorus": [0.3, 0.7],
+      "omega3": [0.1, 1.0],
+      "omega6": [2, 10]
+    }
+  },
+  {
+    "mode": "hnízdění",
+    "adjustments": {
+      "protein": [16, 22],
+      "fat": [8, 18],
+      "calcium": [2, 3.5],
+      "phosphorus": [0.8, 1.2],
+      "omega3": [0.3, 1.5],
+      "omega6": [4, 12]
+    }
+  },
+  {
+    "mode": "línání",
+    "adjustments": {
+      "protein": [14, 20],
+      "fat": [7, 15],
+      "fiber": [6, 12],
+      "omega3": [0.2, 1.2]
+    }
+  },
+  {
+    "mode": "rekonvalescence",
+    "adjustments": {
+      "protein": [15, 25],
+      "fat": [10, 18],
+      "calcium": [1.5, 2.5],
+      "omega3": [0.5, 2.0],
+      "omega6": [3, 12]
+    }
+  },
+  {
+    "mode": "hepatopatie",
+    "adjustments": {
+      "fat": [3, 6],
+      "fiber": [8, 14],
+      "omega3": [0.8, 2.5],
+      "omega6": [1, 4],
+      "calcium": [1.0, 1.5]
+    }
+  },
+  {
+    "mode": "zima",
+    "adjustments": {
+      "fat": [10, 20],
+      "fiber": [6, 10],
+      "energy": [400, 600]
+    }
+  }
+]
+
